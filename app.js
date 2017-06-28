774b3251daa724143593cdb3b836d5437c58ec29
Zach Payton <paytonzk@rose-hulman.edu>
2017-06-28 16:00:46 -0400

    Setup submit handler

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = {
+    init: function(){
+        document.querySelector('formSelector').addEventListener('submit', this.handleSubmit)
+    },
+    handleSubmit: function(ev){
+        ev.preventDefault()
+        const f = ev.target
+        console.log(f.flickName.value)
+    },
+}
+
+app.init('#flick-form')
